4b5f5946e6f9daf6dc62a94205c8ffc86c845d82
Shawn McMillian <shawn.mcmillian@docusign.com>
2017-06-05 10:06:15 -0700

    changed the script that finds files and now allows us to delete the files and empty folders

--- Powershell/Find files older than X days.ps1
+++ Powershell/Find files older than X days.ps1
@@ -1,3 +1,11 @@
-$limit = (Get-Date).AddDays(-25)
+$limit = (Get-Date).AddDays(-22)
 $path = "D:"
+
+# Display all files older than the limit data above, in a Grid view
 Get-ChildItem -Path $path -include *.bak, *.trn -Recurse -ErrorAction SilentlyContinue | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $limit } | select-object FullName, CreationTime, Length | Out-GridView
+
+#Delete all files older than the limit data above, Whatif will have to be removed (for Safety)
+Get-ChildItem -Path $path -include *.bak, *.trn -Recurse -ErrorAction SilentlyContinue | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $limit } | Remove-Item -WhatIf
+
+# Delete any empty directories left behind after deleting the old files.
+Get-ChildItem -Path $path -Recurse | Where-Object { $_.PSIsContainer -and (Get-ChildItem -Path $_.FullName -Recurse | Where-Object { !$_.PSIsContainer }) -eq $null } | Remove-Item -Recurse -Whatif
