88184ddab6ef46b1f5e6ddb4e7eeecbfc1ca610b
Shawn McMillian <shawn.mcmillian@docusign.com>
2018-09-22 14:20:55 -0700

    Adding the combine powershell file

---
+++ Powershell/Combine multiple files into one.ps1
@@ -0,0 +1,9 @@
+﻿$Path = "C:\Users\shawn.mcmillian\Downloads\SQLScripts\"
+$CombineFile = Join-Path -Path $Path -ChildPath "CombinedFiles.sql"
+cls
+
+foreach ($f in Get-ChildItem -path $Path -Filter *.sql | sort-object) 
+{ 
+     $FileToRead = Join-Path -Path $Path -ChildPath $f.Name
+    Get-Content -Path $FileToRead | Out-File -filepath $CombineFile -Append
+}
